e2fc735a1dc2e9cef96fe2b05afc56679ee505f4
ArturoGarIba <chinowarvevo1@gmail.com>
2023-01-20 16:05:17 -0600

    modificacion de charts

--- charts/charts.py
+++ charts/charts.py
@@ -5,6 +5,7 @@ def generate_pie_chart():
     labels = ['A', 'B', 'C']
     values = [200, 34, 120]
 
-    fig, ax, plt.subplots()
+    fig, ax = plt.subplots()
     ax.pie(values, labels=labels)
-    plt.show()
+    plt.savefig('pie.png')
+    plt.close()
